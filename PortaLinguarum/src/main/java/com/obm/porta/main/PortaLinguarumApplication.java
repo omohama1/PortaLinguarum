d5c44f817730faad9aa562087e883b97866a547b
Omar Mohamad <omarbmohamad@gmail.com>
2020-11-11 17:30:37 -0500

    PortaLinguarum - project setup

---
+++ PortaLinguarum/src/main/java/com/obm/porta/main/PortaLinguarumApplication.java
@@ -0,0 +1,13 @@
+package com.obm.porta.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortaLinguarumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortaLinguarumApplication.class, args);
+	}
+
+}
---
+++ PortaLinguarum/src/test/java/com/obm/porta/main/PortaLinguarumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.obm.porta.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortaLinguarumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
